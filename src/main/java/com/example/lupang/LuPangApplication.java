65f26a0f411bf653871dffdd26b9138168fa2e75
dongpani <epyruskdh@gmail.com>
2022-05-10 23:09:47 +0900

    Create new project

---
+++ src/main/java/com/example/lupang/LuPangApplication.java
@@ -0,0 +1,13 @@
+package com.example.lupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuPangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuPangApplication.class, args);
+	}
+
+}
