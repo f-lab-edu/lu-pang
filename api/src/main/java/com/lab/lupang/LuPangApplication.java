3efcdf3fa47ebd3f11cef45bb5bcd134f6ac2028
dhkim <epyruskdh@gmail.com>
2022-05-17 15:02:12 +0900

    기존 모듈제거(payment), 모듈명변경(search -> api), gitignore 불필요 설정 제거

--- api/src/main/java/com/lab/lupang/LuPangApplication.java
+++ api/src/main/java/com/lab/lupang/LuPangApplication.java
@@ -1,13 +1,11 @@
-package com.example.lupang;
+package com.lab.lupang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LuPangApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LuPangApplication.class, args);
 	}
-
 }
